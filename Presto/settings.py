d984ee911bee23f9103695c09bc1e5398fc6e4cd
Rawad Karam <rawad.karam@mail.mcgill.ca>
2018-02-25 23:43:31 -0500

    sdfs

--- Presto/settings.py
+++ Presto/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
