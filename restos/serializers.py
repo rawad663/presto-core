f4745de2a5b7fce5132a0ebe1852fa29a62519a4
Ahmet Akgul <ahmet.akgul@mail.mcgill.ca>
2018-02-28 16:50:53 -0500

    More sauce

--- restos/serializers.py
+++ restos/serializers.py
@@ -25,8 +25,8 @@ class RegistrationSerializer(serializers.ModelSerializer):
         user = User.objects.create_user(
             username=validated_data['username'],
             email=validated_data['email'],
-            first_name=validated_data['firstName'],
-            last_name=validated_data['lastName']
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
         )
         user.set_password(validated_data['password'])
         user.save()
