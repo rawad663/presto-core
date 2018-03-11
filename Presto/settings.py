370d2eab93da217f5cfaea2b477465d90035517b
rawad663 <rawad.karam663@gmail.com>
2018-03-10 19:23:53 -0500

    fnfr

--- Presto/settings.py
+++ Presto/settings.py
@@ -88,8 +88,6 @@ DATABASES = {
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
-DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
