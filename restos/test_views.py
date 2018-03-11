d54270d73eba1183dc15d830b9ec105e80929514
Sabrinel Kadir <sabrinelkadir@Sabrinels-iMac.local>
2018-03-11 14:43:07 -0400

    updated tests for both customers and restaurants registration

--- restos/test_views.py
+++ restos/test_views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.test import TestCase, RequestFactory
 from django.core.urlresolvers import reverse
+from django.contrib.messages.middleware import MessageMiddleware
+from django.contrib.sessions.middleware import SessionMiddleware
 
 from .views import *
 from .models import *
@@ -11,23 +13,18 @@ class RegistrationViewTests(TestCase):
 
     def setUp(self):
       # Every test needs access to the request factory.
-        self.factory = RequestFactory()
+    
         self.u1 = User.objects.create(username='User1', first_name='Alice', last_name='Smith', email='user1@foo.com', password='pass', is_resto=True)
         self.u1.save()
         self.u2 = User.objects.create_user(username='User2', first_name='Bob', last_name='Frank', email='user2@foo.com', password='pass', is_resto=False)
         self.u2.save()
         self.u3 = Resto.objects.create(user=self.u1, created='2017-03-03', resto_name = "Alice's dinner", description='great food', phone_number="12345678", postal_code="H1H2H3")
         self.u3.save()
-        self.u4 = Customer.objects.create(user=self.u2)
+        self.u4 = Customer.objects.create(user=self.u2, liked_restos='')
 
-        def setup_request(self, request):
-            request.user = self.u3
-            request.session.save()
-    
-
-    def test_registration__resto_view(self):
-        url = reverse('register_resto')
-        form_data = {
+    def test_registration_resto_view(self):
+         url = reverse('register_resto')
+         form_data= {
             'resto_name': "Alice's dinner",
             'description': 'great food',
             'phone_number': "12345678",
@@ -35,16 +32,36 @@ class RegistrationViewTests(TestCase):
             'username': 'User1',
             'email': 'user1@foo.com',
             'first_name': 'Alice',
-            'last_name': 'Smith',
+            'last_name': 'Smith', }
+request = RequestFactory().post(url, data=form_data)
+
+SessionMiddleware().process_request(request)
+MessageMiddleware().process_request(request)
+view = RegisterResto.as_view()
+response=view(request)
+
+self.failUnlessEqual(response.status_code, status.HTTP_201_CREATED)
+self.assertEqual(Resto.objects.count(), 0)  
+
+
+def test_register_customer_view(self):
+        url= reverse('register_customer')
+        form_data= {
+            'username': 'User2',
+            'email': 'user2@foo.com',
+            'first_name': 'Bob',
+            'last_name': 'Frank',
         }
+request = RequestFactory().post(url, data=form_data)
 
-        request = self.factory.post(url, data=form_data)
-        self.setup_request(request)
+SessionMiddleware().process_request(request)
+MessageMiddleware().process_request(request)
+view = RegisterCustomer.as_view()
+response=view(request)
 
-        view = RegisterResto.as_view()
-        response = view(request)
-        self.failUnlessEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Resto.objects.count(), 0)   
+self.failUnlessEqual(response.status_code, status.HTTP_201_CREATED)
+self.assertEqual(Customer.objects.count(), 0)  
+
 
 
         
