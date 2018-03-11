e70136b5bdaebed74f93bcdd116dbe49d2ebb6e1
Rony Azrak <rony.azrak@gmail.com>
2018-03-10 21:18:00 -0500

    test fix db

--- Presto/settings.py
+++ Presto/settings.py
@@ -40,9 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restos.apps.RestosConfig',
     'rest_framework',
     'rest_framework.authtoken',
-    'restos.apps.RestosConfig',
 ]
 
 MIDDLEWARE = [
