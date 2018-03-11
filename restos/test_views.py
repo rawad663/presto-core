51642a1e7840d11437cb21fa81c49670886532dc
Sabrinel Kadir <sabrinelkadir@Sabrinels-iMac.local>
2018-03-11 13:59:30 -0400

    updated test registration view

--- restos/test_views.py
+++ restos/test_views.py
@@ -1,32 +1,50 @@
 from django.contrib.auth.models import User
 from django.test import TestCase, RequestFactory
+from django.core.urlresolvers import reverse
 
-from .views import Register, RestoList
+from .views import *
+from .models import *
 
 
-class RegistrationViewTests(RegistrationTestCase):
+class RegistrationViewTests(TestCase):
 
-    def test_registration_view(self):
+
+    def setUp(self):
+      # Every test needs access to the request factory.
+        self.factory = RequestFactory()
+        self.u1 = User.objects.create(username='User1', first_name='Alice', last_name='Smith', email='user1@foo.com', password='pass', is_resto=True)
+        self.u1.save()
+        self.u2 = User.objects.create_user(username='User2', first_name='Bob', last_name='Frank', email='user2@foo.com', password='pass', is_resto=False)
+        self.u2.save()
+        self.u3 = Resto.objects.create(user=self.u1, created='2017-03-03', resto_name = "Alice's dinner", description='great food', phone_number="12345678", postal_code="H1H2H3")
+        self.u3.save()
+        self.u4 = Customer.objects.create(user=self.u2)
+
+        def setup_request(self, request):
+            request.user = self.u3
+            request.session.save()
     
-    
-        response = self.client.post(reverse('Register'),
-                                    data={ 'username': 'alice123', 
-                                           'email': 'foo@example.com',
-                                           'password': 'foo',
-                                           'first_name': 'alice',
-                                           'last_name': 'smith'})
-        self.assertEqual(response.status_code, 200)
-    
 
-       response = self.client.post(reverse('Register'),
-                                    data={ 'username': 'alice123', 
-                                           'email': 'foo@example.com',
-                                           'password': 'foo',
-                                           'first_name': 'alice',
-                                           'last_name': 'smith'})
-        self.assertEqual(response.status_code, 302)
+    def test_registration__resto_view(self):
+        url = reverse('register_resto')
+        form_data = {
+            'resto_name': "Alice's dinner",
+            'description': 'great food',
+            'phone_number': "12345678",
+            'postal_code': "H1H2H3",
+            'username': 'User1',
+            'email': 'user1@foo.com',
+            'first_name': 'Alice',
+            'last_name': 'Smith',
+        }
 
+        request = self.factory.post(url, data=form_data)
+        self.setup_request(request)
 
-    def test_restolist_view(self):
+        view = RegisterResto.as_view()
+        response = view(request)
+        self.failUnlessEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Resto.objects.count(), 0)   
+
 
         
--- restos/urls.py
+++ restos/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     url(r'^restos/([0-9]+)/$', views.RestoDetail.as_view()),
     url(r'^customers/([0-9]+)/$', views.CustomerDetail.as_view()),
     url('register/customer/', views.RegisterCustomer.as_view()),
-    url('register/resto/', views.RegisterResto.as_view()),
+    url('register/resto/', views.RegisterResto.as_view(), name='register_resto'),
     url('like-resto/([0-9]+)/', views.LikeResto.as_view()),
     url('dislike-resto/([0-9]+)/', views.DislikeResto.as_view()),
     url('reserve/([0-9]+)/([0-9]+)/', views.MakeReservation.as_view()),
